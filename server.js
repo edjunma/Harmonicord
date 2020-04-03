efd38bd4f0544018e6abb80723f1b532eee7038b
edjunma <edjunma@gmail.com>
2020-04-03 00:00:32 -0400

    finish with server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 // Set static folder
-app.use(express.static())
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
